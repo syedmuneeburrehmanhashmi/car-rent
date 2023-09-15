e65d4e3451b6183b82f1b70baa5cec91c4a8b7a4
Syed Muneeb Ur Rehman Hashmi <syed.muneeb@synergates.com>
2023-09-15 17:06:08 +0500

    footer component added

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InfoBannerComponent } from './info-banner/info-banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    InfoBannerComponent
+    InfoBannerComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    InfoBannerComponent
+    InfoBannerComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
